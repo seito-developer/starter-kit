bbea567de0a871c7b27cbb4c2b38878c7d69f5f2
yamato2023 <seito@uni.minerva.edu>
2023-03-30 19:13:22 +0900

    w6-starter-kit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+class Person {
+	constructor(obj){
+        this.name = obj.name;
+        this.age = obj.age;
+        this.friend = obj.friend;
+    }
+
+    sayHelloToFriend() {
+      document.getElementById("voice").innerHTML = `Hi, ${this.friend}`;
+    }
+}
+
+const Mike = new Person({
+  name: "Mike",
+  age: 20,
+  friend: "Mayu"
+});
+
+Mike.sayHelloToFriend();
